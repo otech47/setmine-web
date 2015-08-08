4d4e79401ebdf740a72b23384c86486a41183f86
quinnstinchurchill <quinnpruitt@ufl.edu>
2015-08-08 01:17:13 -0400

    change location

--- public/js/src/components/FeaturedResultsHeader.js
+++ public/js/src/components/FeaturedResultsHeader.js
@@ -1,6 +1,17 @@
 var React = require('react')
 
 var FeaturedResultsHeader = React.createClass({
+    getInitialState: function() {
+        return {
+            location: 'Dania Beach, FL, USA' 
+        };
+    },
+    getNewLocation: function() {
+        var newLocation = prompt('Enter your location: ');
+        this.setState({
+            location: newLocation 
+        });
+    },
 	render: function() {
 		return (
 			<div className="flex-row featured-results-header">
@@ -8,8 +19,8 @@ var FeaturedResultsHeader = React.createClass({
                 <div className="buffer-2x"></div>
                 <div className="flex center flex-row">
                     <i className="flex fa fa-map-marker"></i>
-                    <div className="flex user-location">Dania Beach, FL, USA</div>
-                    <div className="flex change-location">Change</div>
+                    <div className="flex user-location">{this.state.location}</div>
+                    <div className="flex change-location" onClick={this.getNewLocation}>Change</div>
                 </div>
             </div>
 		);
