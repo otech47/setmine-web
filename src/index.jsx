0f34df06ceb144690c8da902e2180652e2459497
Rafael Khan <raf.khan.f@gmail.com>
2015-08-21 16:31:45 -0400

    Fix linting

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Router from 'react-router';
-import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+import { DefaultRoute, Route, RouteHandler } from 'react-router';
 
-var constants = require('./constants/constants');
+//var constants = require('./constants/constants');
 
-var Player = require('./components/Player');
-var Footer = require('./components/Footer');
+//var Player = require('./components/Player');
+//var Footer = require('./components/Footer');
 var Header = require('./components/Header');
 var DetailView = require('./components/DetailView');
 var LandingView = require('./components/LandingView');
@@ -14,13 +14,12 @@ var FeaturedView = require('./components/FeaturedView');
 var HomeView = require('./components/HomeView');
 var SearchResultsView = require('./components/SearchResultsView');
 
-console.log(SearchResultsView);
 
-var SetTile = require('./components/SetTile');
-var EventTile = require('./components/EventTile');
-var TrackTile = require('./components/TrackTile');
+//var SetTile = require('./components/SetTile');
+//var EventTile = require('./components/EventTile');
+//var TrackTile = require('./components/TrackTile');
 
-var viewStream = require('./streams/viewStream');
+//var viewStream = require('./streams/viewStream');
 
 // <FeaturedView landingEvents={landing} currentEvents={sampleCurrentEvents}/>
 // <DetailView artistId={574} detailType='artist'/>
@@ -30,6 +29,7 @@ var viewStream = require('./streams/viewStream');
 //call setState which pushes to event stream when receiving an event
 
 var App = React.createClass({
+  displayName: 'App container',
 	getInitialState: function() {
 		return {
 			searchInput: '',
@@ -37,17 +37,16 @@ var App = React.createClass({
 			userLoggedIn: false,
 		};
 	},
-	_attachStream: function() {
-		var _this = this;
+	//_attachStream: function() {
+		//var _this = this;
 
-		function updateActiveView (view) {
-			_this.setState({
-				activeView: view
-			});
-		}
-	},
+		//function updateActiveView (view) {
+			//_this.setState({
+				//activeView: view
+			//});
+		//}
+	//},
 	render: function() {
-		var activeView = this.state.activeView;
 		return (
 			<div className="main-container flex-column">
 				<Header />
@@ -58,34 +57,24 @@ var App = React.createClass({
 });
 
 
-var App2 = React.createClass({
-	render: function() {
-
-		return (<div>
-					<h1> FUCK </h1>
-					<RouteHandler />
-				 </div>);
-	}
-});
-
 var routes = (
-	<Route path='/' handler={App}>
+	<Route path='/'handler={App}>
+		<DefaultRoute handler={SearchResultsView} />
 		<Route path='home' handler={LandingView} />
-		<Route path='browse' handler={BrowseView}></Route>
+		<Route path='browse' handler={BrowseView} />
 		<Route path='featured' handler={FeaturedView} />
 		<Route path='user' handler={HomeView} />
-		<DefaultRoute handler={SearchResultsView} />
 		<Route path='artist/:id' handler={DetailView} />
 		<Route path='event/:id' handler={DetailView} />
 	</Route>
 );
 
-var headMount = document.getElementById('head-mount-point');
+//var headMount = document.getElementById('head-mount-point');
 var bodyMount = document.getElementById('body-mount-point');
 
 Router.run(routes, Router.HashLocation, (Root) => {
 	console.log(Root);
 	React.render(<Root/>, bodyMount);
-})
+});
 
 // React.render(<App />, bodyMount);
