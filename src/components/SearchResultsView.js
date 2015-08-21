40ae4a5e43193cac0e6631d7990902fdd49683e8
quinnstinchurchill <quinnpruitt@ufl.edu>
2015-08-21 15:17:41 -0400

    results

---
+++ src/components/SearchResultsView.js
@@ -0,0 +1,32 @@
+var React = require('react')
+var Router = require('react-router')
+var Route = Router.Route;
+var Router = require('react-router')
+
+var SearchResultsView = React.createClass({
+	componentDidMount: function() {
+		//this._attachStream();
+	},
+	render: function() {
+		return (
+			<div id="search-results" className="view overlay-container hidden">
+	          <div className="flex-row view-title-container sets">
+					<div className="view-title sets results-filter flex set-flex">
+						<div className="center">Sets</div>
+					</div>
+					<div className="view-title events results-filter flex set-flex">
+						<div className="center">Events</div>
+					</div>
+					<div className="view-title tracks results-filter flex set-flex">
+					  	<div className="center">Tracks</div>
+					</div>
+					<div className="divider"></div>
+	          </div>
+				<div className="search-results">
+				</div>
+          </div>
+		);
+	}
+});
+
+module.exports = SearchResultsView;
