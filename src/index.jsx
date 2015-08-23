17f7f7e7a3cde35549874d3b7f82ceaa0a4beaf1
quinnstinchurchill <quinnpruitt@ufl.edu>
2015-08-23 19:08:00 -0400

    merge fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,21 @@
 import React from 'react';
 import Router from 'react-router';
-<<<<<<< HEAD
 import { DefaultRoute, Link, Route, RouteHandler, Navigation } from 'react-router';
-var constants = require('./constants/constants');
-=======
-import { DefaultRoute, Route, RouteHandler } from 'react-router';
-
+import constants from './constants/constants';
 import GlobalEventHandler from './globalEventHandler';
->>>>>>> master
 
-// TODO use ES6 imports moving forward
-//var constants = require('./constants/constants');
+import Footer from './components/Footer';
+import Header from './components/Header';
+import DetailView from './components/DetailView';
+import LandingView from './components/LandingView';
+import BrowseView from './components/BrowseView';
+import FeaturedView from './components/FeaturedView';
+import HomeView from './components/HomeView';
+import SearchResultsView from './components/SearchResultsView';
 
-//var Player = require('./components/Player');
-//var Footer = require('./components/Footer');
-var Header = require('./components/Header');
-var DetailView = require('./components/DetailView');
-var LandingView = require('./components/LandingView');
-var BrowseView = require('./components/BrowseView');
-var FeaturedView = require('./components/FeaturedView');
-var HomeView = require('./components/HomeView');
-var SearchResultsView = require('./components/SearchResultsView');
-
-<<<<<<< HEAD
-var SetTile = require('./components/SetTile');
-var EventTile = require('./components/EventTile');
-var TrackTile = require('./components/TrackTile');
-
-=======
-
-//var SetTile = require('./components/SetTile');
-//var EventTile = require('./components/EventTile');
-//var TrackTile = require('./components/TrackTile');
-
-//var viewStream = require('./streams/viewStream');
-
-// <FeaturedView landingEvents={landing} currentEvents={sampleCurrentEvents}/>
-// <DetailView artistId={574} detailType='artist'/>
-
->>>>>>> master
-//subscribe in componentDidMount()
-//unsubscribe in componentWillUnmount()
-//call setState which pushes to event stream when receiving an event
-//
+import SetTile from './components/SetTile';
+import EventTile from './components/EventTile';
+import TrackTile from './components/TrackTile';
 
 var evtHandler = GlobalEventHandler({hola:'world'});
 var evtTypes = evtHandler.types;
@@ -51,72 +24,58 @@ var push = evtHandler.push;
 
 
 function lol() {
-  push({
-    type: evtTypes.SHALLOW_MERGE,
-    data: { lastClick: new Date() }
-  });
+	push({
+		type: evtTypes.SHALLOW_MERGE,
+		data: { lastClick: new Date() }
+	});
 }
 
 
 var PrintObject = React.createClass({
-  displayName: 'PrintObject',
-  render: function() {
-    var s = JSON.stringify(this.props.value, null, 2);
-    console.log('PO APP STATE', this.props.value);
-    return React.createElement('code', {
-      style: { fontSize: 10 },
-      onClick: lol
-    }, s);
-  }
+	displayName: 'PrintObject',
+	render: function() {
+		var s = JSON.stringify(this.props.value, null, 2);
+		console.log('PO APP STATE', this.props.value);
+		return React.createElement('code', {
+			style: { fontSize: 10 },
+			onClick: lol
+		}, s);
+	}
 });
 
 
 var App = React.createClass({
-  displayName: 'App container',
-
+	displayName: 'App container',
 	getInitialState: function() {
 		return {
-		  // Let's assume that other ephemeral state
-		  // MAY have to exist here.
-		  appState: {}
+			// Let's assume that other ephemeral state
+			// MAY have to exist here.
+			appState: {}
 		};
 	},
-<<<<<<< HEAD
-	mixins: [Navigation],
+	componentDidMount: function() {
+		this._attachStreams();
+	},
+	_attachStreams: function() {
+		var _this = this;
+		evtHandler.floodGate.subscribe(newState => {
+			_this.setState({ appState: newState });
+		});
+	},
 	render: function() {
-		return (
-			<div className="main-container flex-column">
-				<Header />
-				<RouteHandler />
-				<Footer />
-=======
-
-  componentDidMount: function() {
-    this._attachStreams();
-  },
-
-  _attachStreams: function() {
-    var _this = this;
-    evtHandler.floodGate.subscribe(newState => {
-      _this.setState({ appState: newState });
-    });
-  },
-
-	render: function() {
-	  var appState = this.state;
+		var appState = this.state;
 
 		return (
 			<div className="main-container flex-column">
-			  <PrintObject value={appState} />
+				<PrintObject value={appState} />
 				<Header appState={appState} pushFn={push} />
 				<RouteHandler appState={appState} pushFn={push} />
->>>>>>> master
+				<Footer />
 			</div>
 		);
 	}
 });
 
-<<<<<<< HEAD
 var routes = (
 	<Route path='/' handler={App}>
 		<DefaultRoute name='landing' handler={LandingView} />
@@ -130,18 +89,6 @@ var routes = (
 		<Route name='event' path='event' handler={DetailView}>
 			<Route path=':id'/>
 		</Route>
-=======
-
-var routes = (
-	<Route path='/'handler={App}>
-		<DefaultRoute handler={SearchResultsView} />
-		<Route path='home' handler={LandingView} />
-		<Route path='browse' handler={BrowseView} />
-		<Route path='featured' handler={FeaturedView} />
-		<Route path='user' handler={HomeView} />
-		<Route path='artist/:id' handler={DetailView} />
-		<Route path='event/:id' handler={DetailView} />
->>>>>>> master
 	</Route>
 );
 
