4817b277808315350c6d660836d3e4e9c1f9eab9
James Risberg <james.risberg@gmail.com>
2018-04-12 15:34:49 -0400

    Add CoinPayments button to accept cryptocurrency

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -13,9 +13,17 @@ export default class LandingPage extends Base {
     render() {
         return (
             <div className='LandingPage'>
-                <p>This is the LandingPage component</p>
+                <p>Please give us money</p>
                 
-                
+                <form action="https://www.coinpayments.net/index.php" method="post">
+                    <input type="hidden" name="cmd" value="_donate"/>
+                    <input type="hidden" name="reset" value="1"/>
+                    <input type="hidden" name="merchant" value="6fb2cdcc838888540ff0d5a702375316"/>
+                    <input type="hidden" name="currency" value="USD"/>
+                    <input type="hidden" name="amountf" value="20.00"/>
+                    <input type="hidden" name="item_name" value="Test Donation"/>		
+                    <input type="image" src="https://www.coinpayments.net/images/pub/donate-grey.png" alt="Donate with CoinPayments.net"/>
+                </form>
                 
             </div>
         );
