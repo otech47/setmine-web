6ef7e6891db1de26da93d521c05dbcd9cc8753e7
Oscar Lafarga <otech47@gmail.com>
2019-04-18 10:52:34 +0200

    Remove broken package

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,13 @@
 import React, {PropTypes} from 'react';
 import Ink from 'react-ink';
-import classNames from 'classNames';
 import Icon from './Icon';
 
 const {func, string, bool, object} = PropTypes;
 
 export default function Button({onClick, className, children, outline, icon, solid}, {router, push}) {
-    const buttonClass = classNames('Button', className, {
-        solid: solid,
-        outline: outline
-    });
 
     return (
-        <div className={buttonClass} onClick={() => setTimeout(onClick, 100)}>
+        <div className={'Button' + (solid ? ' solid' : '') + (outline ? ' outline' : '')} onClick={() => setTimeout(onClick, 100)}>
             {icon && <Icon style={{marginRight: '1rem' }}>{icon}</Icon>}
             <p>{children}</p>
             <Ink/>
